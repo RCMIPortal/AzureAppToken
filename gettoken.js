067d7591a86c539bc22ea70e1efb18d63a208c71
Chazza <ch@rli.es>
2021-01-26 14:59:23 +0000

    Added code

---
+++ gettoken.js
@@ -0,0 +1,53 @@
+const request = require('request');
+const { client_id, client_secret, tenant } = require('./azure.json')
+
+let accessToken = ''
+
+function generateToken() {
+    const options = {
+        method: 'POST',
+        url: `https://login.microsoftonline.com/${tenant}/oauth2/v2.0/token`,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        form: {
+            client_id: client_id,
+            client_secret: client_secret,
+            grant_type: 'client_credentials',
+            scope: 'https://graph.microsoft.com/.default'
+        },
+        json: true
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        accessToken = body.access_token
+        console.log(`> Generated token: ${accessToken}`)
+        console.log(' ')
+        console.log(`> Preparing to fetch users..`)
+        fetchUsers()
+    });
+}
+
+function fetchUsers() {
+    const options = {
+        method: 'GET',
+        url: `https://graph.microsoft.com/v1.0/users/${tenant}`,
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json;odata.metadata=minimal;odata.streaming=true;IEEE754Compatible=false;charset=utf-8'
+        }
+    };
+
+    request(options, function (error, response, body) {
+        if (error || body.error) {
+            throw new Error(error || body.error.message);
+        }
+
+        console.log(body);
+    });
+
+}
+
+generateToken();
